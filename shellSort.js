21761d546b6449d93a5343104a83de5670b94467
Patrick <patrick.luboobi@andela.com>
2018-11-11 21:53:19 +0300

    shellsort

--- shellSort.js
+++ shellSort.js
@@ -1,17 +1,27 @@
 // Split the array into halves and merge them recursively 
-function shellSort (arr) {
-	let interval = 4;
-	while( interval >= 1){
-		let sublist = [];
-		for (let i = 0; i < arr.length; i++) {
-			if(i % interval === 0 ) {
-				sublist.push(arr[i])
+function shellSort(arr) {
+	let increment = arr.length / 2;
+	while (increment > 0) {
+		for (i = increment; i < arr.length; i++) {
+			let j = i;
+			let temp = arr[i];
+
+			while (j >= increment && arr[j-increment] > temp) {
+				arr[j] = arr[j-increment];
+				j = j - increment;
 			}
+
+			arr[j] = temp;
 		}
-		console.log(interval, "====", arr);
-		interval = interval / 2
+
+		if (increment === 2) {
+			increment = 1;
+		} else {
+			increment = parseInt(increment * 5 / 11);
+		}
 	}
 	return arr;
 }
+
 
 console.log(shellSort([35, 33, 42, 10, 14, 19, 27, 44]));
